8ba984670febd21979f5c9f5c5157e4849f73c62
Šimon Prokopec <prokopec.simon@nvsp.cz>
2023-08-05 13:44:43 +0200

    Getting AuctionDB buyout filter prototype

--- goldbrew.lua
+++ goldbrew.lua
@@ -4,24 +4,39 @@ button:SetText("Debugging btn")
 
 local function OnButtonClick()
     local myAddonData = AuctionDBSaved["ah"][1]["data"]
-    local auctions = splitAuctions(myAddonData)
+    local itemIDs = {
+        ["7473"] = true,
+        ["14218"] = true
+    }
+    local auctions = splitAuctions(myAddonData, itemIDs)
 
     for _, auction in ipairs(auctions) do
-        print("V4Key: " .. auction.v4key .. ", Buyout Price: " .. auction.buyoutPrice)
+        print("Item ID: " .. auction.itemID .. ", Buyout Price: " .. auction.buyoutPrice)
     end
 
 end
 
 button:SetScript("OnClick", OnButtonClick)
 
-function splitAuctions(dataString)
+function splitAuctions(dataString, itemIDs)
     local auctions = {}
-    for v4key, seller, buyoutPrice in dataString:gmatch("(i%d+%?.-)%!/(.-)%,%d+,%d+,(%d+),.-") do
-        seller = seller or ""
-        table.insert(auctions, {
-            v4key = v4key .. "!" .. seller,
-            buyoutPrice = tonumber(buyoutPrice)
-        })
+    for v4key, _, buyoutPrice in dataString:gmatch("(i%d+%?.-)%!/(.-)%,%d+,%d+,(%d+),.-") do
+        local itemID = v4key:match("i(%d+)")
+        if not itemIDs or itemIDs[itemID] then
+            table.insert(auctions, {
+                itemID = itemID,
+                buyoutPrice = tonumber(buyoutPrice)
+            })
+        end
     end
     return auctions
+end
+
+local function has_value(tab, val)
+    for index, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+    return false
 end
