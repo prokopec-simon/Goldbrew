c8db93d2fba585dbd5aa77a2b9a62d372a3e404d
Šimon Prokopec <prokopec.simon@nvsp.cz>
2023-08-12 22:45:19 +0200

    Split core into services

--- goldbrew.lua
+++ goldbrew.lua
@@ -3,39 +3,8 @@ button:SetPoint("RIGHT")
 button:SetText("Debugging btn")
 
 local function OnButtonClick()
-    local myAddonData = AuctionDBSaved["ah"][1]["data"]
-    local itemIDs = {
-        ["7473"] = true,
-        ["14218"] = true
-    }
-    local auctions = splitAuctions(myAddonData, itemIDs)
-
-    for _, auction in ipairs(auctions) do
-        print("Item ID: " .. auction.itemID .. ", Buyout Price: " .. auction.buyoutPrice)
-    end
-
-    local item = getInventoryContentsFromBagnon()
-
-    print("Item: " .. item)
+    local herbsByIdWithAmount = getHerbsFromBags();
+    local auctionPrices = getAuctionPricesForAvailableMaterials(herbsByIdWithAmount)
 end
 
 button:SetScript("OnClick", OnButtonClick)
-
-function splitAuctions(dataString, itemIDs)
-    local auctions = {}
-    for v4key, _, buyoutPrice in dataString:gmatch("(i%d+%?.-)%!/(.-)%,%d+,%d+,(%d+),.-") do
-        local itemID = v4key:match("i(%d+)")
-        if not itemIDs or itemIDs[itemID] then
-            table.insert(auctions, {
-                itemID = itemID,
-                buyoutPrice = tonumber(buyoutPrice)
-            })
-        end
-    end
-    return auctions
-end
-
-function getInventoryContentsFromBagnon()
-    local myAddonData = BrotherBags["Ashbringer"]["Ahpeon"][0][1]
-    return myAddonData
-end
